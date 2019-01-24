b54857c4c5f8a8a99bea320a06cf4f6a6dce0232
peterhchen <peter.hp.chen@gmail.com>
2019-01-23 16:51:53 -0800

    Interface

---
+++ src_ch06/MammalIntProj/src/MammalInt.java
@@ -0,0 +1,16 @@
+public class MammalInt implements Animal {
+    public void eat () {
+        System.out.println ("Mammal easts.");
+    }
+    public void travel () {
+        System.out.println ("Mammal travels.");
+    }
+    public int noOfLegs () {
+        return 0;
+    }
+    public static void main (String[] args) {
+        MammalInt m = new MammalInt ();
+        m.eat();
+        m.travel();
+    }
+}
